9e9f5cfc5edbd0614f5a64f86489da709110bb29
Linshiq <284244633@qq.com>
2019-05-16 10:45:17 +0800

    补充说明test使用方式

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,22 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.util.CompareUtil;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private CompareUtil compareUtil;
+	
 	@Test
 	public void contextLoads() {
+		System.out.println("TEST class " + compareUtil.toString());
 	}
 
 }
