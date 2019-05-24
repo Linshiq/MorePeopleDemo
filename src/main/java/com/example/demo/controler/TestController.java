686eb3e61e7fcba84efc684869dfbcf82c2ebbd9
Linshiq <284244633@qq.com>
2019-05-24 10:38:50 +0800

    补充log4j2

--- src/main/java/com/example/demo/controler/TestController.java
+++ src/main/java/com/example/demo/controler/TestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/")
 public class TestController {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public String test() throws Exception {
-	
+		
+		logger.info("测试");
+		
 		return "测试";
 	}
 }
